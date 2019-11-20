eaecfb36080d949be73aae7f65520bd8259e21f1
nelo <nelokwaz@gmail.com>
2019-11-20 19:34:21 +0100

    copied the structure from the legacy view file to the corresponding blade template view file

--- resources/views/user-comments.blade.php
+++ resources/views/user-comments.blade.php
@@ -1,7 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: EZEKIEL
- * Date: 20/11/2019
- * Time: 7:30 PM
- */
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>User Card - <?=$user->name?></title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+<body class="is-preload">
+<div id="wrapper">
+    <section id="main">
+        <header>
+            <span class="avatar"><img src="images/users/<?=$user->id?>.jpg" alt="" /></span>
+            <h1><?=$user->name?></h1>
+            <p><?=nl2br($user->comments)?></p>
+        </header>
+    </section>
+    <footer id="footer">
+        <ul class="copyright">
+            <li>&copy; Pictureworks</li>
+        </ul>
+    </footer>
+
+</div>
+<script>
+    if ('addEventListener' in window) {
+        window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+        document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+    }
+</script>
+</body>
+</html>

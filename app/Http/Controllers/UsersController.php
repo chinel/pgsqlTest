57f54b5d0af6714393e686c50e622e4e6b245fc6
nelo <nelokwaz@gmail.com>
2019-11-20 19:32:24 +0100

    created the function to retrieve users comment, also created its corresponding view file and deleted default view file

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    /*
+     * Function to retrieve a users comment
+     * */
+    public function getUserComments($id){
+        $user = User::findOrFail($id);
+
+        //  return $user['name'];
+        return view('user-comments', compact('user'));
+    }
+
 }

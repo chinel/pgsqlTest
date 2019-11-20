f42e35d7001dac16ef578b5e60052c504bbb981c
nelo <nelokwaz@gmail.com>
2019-11-20 19:39:45 +0100

    formatted the legacy html structure in the view file to laravel format

--- resources/views/user-comments.blade.php
+++ resources/views/user-comments.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>User Card - <?=$user->name?></title>
+    <title>User Card - {{$user['name']}}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
+    <noscript><link rel="stylesheet" href="{{asset('assets/css/noscript.css')}}" /></noscript>
 </head>
 <body class="is-preload">
 <div id="wrapper">
     <section id="main">
         <header>
-            <span class="avatar"><img src="images/users/<?=$user->id?>.jpg" alt="" /></span>
-            <h1><?=$user->name?></h1>
-            <p><?=nl2br($user->comments)?></p>
+            <span class="avatar"><img src="{{asset('images/users/'. $user['id'] .'.jpg')}}" alt="" /></span>
+            <h1>{{$user['name']}}</h1>
+            <p>{!!  str_replace( ',', "<br/>", $user['comments'] ) !!}</p>
         </header>
     </section>
     <footer id="footer">
